3f5536710c50a0d66854b9018329a7179076cb40
Константин Сергеевич Фисун <kostya.ficyn@gmail.com>
2022-09-25 10:09:36 +0300

    Отображение нескольких изображений

--- Laba_2.py
+++ Laba_2.py
@@ -51,13 +51,68 @@ def gauss(n, sigma, img):
                     newVal = newVal + gauss_matrix[k][l] * newGray[i - a + k][j - a + l]
             newGray[i][j] = newVal
 
+    # Отображаем результат с исходным изображением
+    display_images(([gray, newGray]),0.3)
+
+
+def display_images(imgarray,scale):
+    rows = len(imgarray)  # Длина кортежа или списка
+    cols = len(imgarray[
+                   0])  # Если imgarray - это список, вернуть количество каналов первого изображения в списке, если это кортеж, вернуть длину первого списка, содержащегося в кортеже
+    # print("rows=", rows, "cols=", cols)
+
+    # Определить, является ли тип imgarray [0] списком
+    # Список, указывающий, что imgarray является кортежем и должен отображаться вертикально
+    rowsAvailable = isinstance(imgarray[0], list)
+
+    # Ширина и высота первой картинки
+    width = imgarray[0].shape[1]
+    height = imgarray[0].shape[0]
+    # print("width=", width, "height=", height)
+
+    # Если входящий кортеж
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                # Обойти кортеж, если это первое изображение, не преобразовывать
+                if imgarray[x][y].shape[:2] == imgarray[0][0].shape[:2]:
+                    imgarray[x][y] = cv2.resize(imgarray[x][y], (0, 0), None, scale, scale)
+                # Преобразуйте другие матрицы к тому же размеру, что и первое изображение, и коэффициент масштабирования будет масштабироваться
+                else:
+                    imgarray[x][y] = cv2.resize(imgarray[x][y], (imgarray[0][0].shape[1], imgarray[0][0].shape[0]),
+                                                None, scale, scale)
+
+        # Создайте пустой холст того же размера, что и первое изображение
+        imgBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imgBlank] * rows  # Тот же размер, что и первое изображение, и столько же горизонтальных пустых изображений, сколько кортеж содержит список
+        for x in range(0, rows):
+            # Расположить x-й список в кортеже по горизонтали
+            hor[x] = np.hstack(imgarray[x])
+        ver = np.vstack(hor)  # Объединить разные списки по вертикали
+    # Если входящий - это список
+    else:
+        # Операция трансформации, как и раньше
+        for x in range(0, rows):
+            if imgarray[x].shape[:2] == imgarray[0].shape[:2]:
+                imgarray[x] = cv2.resize(imgarray[x], (0, 0), None, scale, scale)
+            else:
+                imgarray[x] = cv2.resize(imgarray[x], (imgarray[0].shape[1], imgarray[0].shape[0]), None, scale, scale)
+        # Расположить список по горизонтали
+        hor = np.hstack(imgarray)
+        ver = hor
+
+    cv2.namedWindow('Display window', cv2.WINDOW_NORMAL)
+    cv2.imshow('Display window', ver)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 def main():
     img = cv2.imread(r'2.jpg')
-
-    n = 7
-    sigma = 1
+    n = 3
+    sigma = 0.3
     gauss(n,sigma,img)
+
+
 
 if __name__ == "__main__":
     main()
